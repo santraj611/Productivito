d9aad6ad92250659c39d58f6da0192c880cf1b6f
Sant Raj <santraj611@gmail.com>
2025-01-04 22:43:46 +0530

    states enabled

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 # Main flask app
 from flask import Flask, render_template, jsonify
 from graphs import generate_graph, generate_monthly_summary, generate_weekly_summary
+from states import get_date, get_week, get_month, total_screen_time
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    today =  str(get_date())
+    week_ago = get_week()
+    month_ago = get_month()
+    total_st = int(total_screen_time(today))
+    this_week = int(total_screen_time(week_ago))
+    this_month = int(total_screen_time(month_ago))
+    return render_template("index.html", today_st=total_st, this_week=this_week, this_month=this_month)
 
 @app.route("/weekly-summary")
 def weekly_summary():
